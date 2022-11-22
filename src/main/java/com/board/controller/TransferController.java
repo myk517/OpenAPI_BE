5314aa35a26738ba3db2629b2e741a8a4a9921b0
myk <koreash321@gmail.com>
2022-11-22 22:22:07 +0900

    출금이체 에러 수정

--- src/main/java/com/board/controller/TransferController.java
+++ src/main/java/com/board/controller/TransferController.java
@@ -1,25 +1,24 @@
 package com.board.controller;
 
-import java.util.HashMap;
-
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.board.domain.BankReponseToken;
 import com.board.domain.transfer.WithdrawRequestDTO;
-import com.board.domain.transfer.WithdrawResponseDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -38,73 +37,58 @@ public class TransferController {
 	@Value("${openbank.base-url}")
 	private String base_url;
 	
+	 
+	
+	
 //	public BankReponseToken brt = new BankReponseToken();
 //	public String accessToken = brt.getAccess_token();
-//	HttpSession session = null;
-	public String accessToken;
+	
 	  //출금이체
     @PostMapping("/withdraw")
-    public HashMap<String, Object> getTokenApi(WithdrawRequestDTO withdrawRequestDto, HttpSession session) {
+    public ResponseEntity<String> withdraw(@RequestBody WithdrawRequestDTO withdrawRequestDto, HttpServletRequest servletReq) {
     	System.out.println("withdraw API .... ");
+    	ResponseEntity<String> response= null;
+    	String url = base_url + "/withdraw/fin_num";
+    	HttpSession session = servletReq.getSession();
+    	session.setAttribute("access_token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMTAxMDEzOTA4Iiwic2NvcGUiOlsiaW5xdWlyeSIsImxvZ2luIiwidHJhbnNmZXIiXSwiaXNzIjoiaHR0cHM6Ly93d3cub3BlbmJhbmtpbmcub3Iua3IiLCJleHAiOjE2NzY4NzU1NDcsImp0aSI6IjkzNzJlZjIwLTFiODYtNDgxNy04NmUyLTM1YWVjYzdlZDJjMSJ9.RIrzBYjbHQNeUd0-w20dUv77C4Ztx9zhpZamwQSFKHM\",\"token_type\":\"Bearer\",\"refresh_token\":\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMTAxMDEzOTA4Iiwic2NvcGUiOlsiaW5xdWlyeSIsImxvZ2luIiwidHJhbnNmZXIiXSwiaXNzIjoiaHR0cHM6Ly93d3cub3BlbmJhbmtpbmcub3Iua3IiLCJleHAiOjE2Nzc3Mzk1NDcsImp0aSI6ImU3YzY4ZmQ3LTIwNzktNDBkYi05MDRmLTE2Y2Y2NjEyNWE5YSJ9.apfBgGr8nWum6-tZBdTf9roCoOXh3e057ePWOnNRKMk" );
   	  // 0. 결과값을 담을 객체를 생성합니다.
-        HashMap<String, Object> resultMap = new HashMap<String, Object>();
-        
         try {
       	  
-      	  // 2. RestTemplate 객체를 생성합니다.
-            RestTemplate restTemplate = new RestTemplate();
-            
             //Header 및 Body 설정
             HttpHeaders headers = new HttpHeaders();
             //http 헤더 오브젝트 생성
-            accessToken = (String) session.getAttribute("accessToken");
             headers.add("Content-Type","application/json;charset=UTF-8");
-            headers.add("Authorization", "Bearer "+accessToken);
-            System.out.println("headers >> " + headers.toString());
-            MultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
-            
-            //파라미터 담기
-            //Date ...현재시간
-            withdrawRequestDto.SetWithdrawRC(bankTranId, "N", "100000000002", "쇼핑몰환불", fintechUseNum, "오픈뱅킹출금","1000",
-            "20201001150133","김미영","004","61250201399911", "HONGGILDONG1234", "RC");
-            
-            System.out.println("::: >>> :: " + withdrawRequestDto.toString());
-            
-            
-	          parameters.add("bank_tran_id", withdrawRequestDto.getBank_tran_id()); 
-	          parameters.add("cntr_account_type", withdrawRequestDto.getCntr_account_type());
-	          parameters.add("cntr_account_num", withdrawRequestDto.getCntr_account_num());
-	          parameters.add("dps_print_content", withdrawRequestDto.getDps_print_content());
-	          parameters.add("fintech_use_num", withdrawRequestDto.getFintech_use_num());
-	          parameters.add("wd_print_content", withdrawRequestDto.getWd_print_content());
-	          parameters.add("tran_amt", withdrawRequestDto.getTran_amt());
-	          parameters.add("tran_dtime", withdrawRequestDto.getTran_dtime());
-	          parameters.add("req_client_name", withdrawRequestDto.getReq_client_name());
-	          parameters.add("req_client_account_num", withdrawRequestDto.getReq_client_account_num());
-	          parameters.add("req_client_num", withdrawRequestDto.getReq_client_num());
-	          parameters.add("transfer_purpose", withdrawRequestDto.getTransfer_purpose());
-            
-	          
-            //HTTP POST 요청
-            String url = base_url + "/transfer/withdraw/fin_num";
+            headers.add("Authorization", "Bearer "+ "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMTAxMDEzOTA4Iiwic2NvcGUiOlsiaW5xdWlyeSIsImxvZ2luIiwidHJhbnNmZXIiXSwiaXNzIjoiaHR0cHM6Ly93d3cub3BlbmJhbmtpbmcub3Iua3IiLCJleHAiOjE2NzY4Nzg5NDMsImp0aSI6IjNjMDMxMjY5LWRmYzgtNGZiNy1iMzkzLTk1NmQyOTRkOGIwZCJ9.4SFjK5wCJmhG3zhE4aj_fJ6v1szJ_ZKOyqeORXeiWMc");
 
-            //설정한 Header와 Body를 가진 HttpEntity 객체 생성
-            // HttpHeader 와 HttpBody를 하나의 오브젝트에 담기
-	          HttpEntity<MultiValueMap<String,String>> request =
-	                  new HttpEntity<>(parameters,headers);
+            MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+            map.add("api_tran_id", "M202201963U008140230}");
+            map.add("cntr_account_type", "N");
+            map.add("cntr_account_num", "100000000002");
+            map.add("dps_print_content", "쇼핑몰환불");
+            map.add("fintech_use_num", fintechUseNum);
+            map.add("wd_print_content", "오픈뱅킹출금");
+            map.add("tran_amt", "1000");
+            map.add("tran_dtime", "20201001150133");
+            map.add("req_client_name", "김미영");
+            map.add("req_client_bank_code", "004");
+            map.add("req_client_account_num", "61250201399911");
+            map.add("req_client_num", "HONGGILDONG1234");
+            map.add("transfer_purpose", "RC");
+//            withdrawRequestDto.SetWithdrawRC(bankTranId, "N", "100000000002", "쇼핑몰환불", fintechUseNum, "오픈뱅킹출금","1000",
+//            "20201001150133","김미영","004","61250201399911", "HONGGILDONG1234", "RC");
+            
+           RestTemplate restTemplate = new RestTemplate();
+           HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
 	          
-	          //Http 요청하기 - post 방식으로
-	          WithdrawResponseDTO res = restTemplate.exchange(url, HttpMethod.POST, request, WithdrawResponseDTO.class).getBody();
-	          
-            //헤더의 컨텐트 타입이 application/x-www-form-urlencoded;charset=UTF-8이므로 객체를 집어넣을수 없음.. 그러므로 MultiValueMap 사용 해야함
-            // 6. 요청한 결과를 HashMap에 추가합니다.
-	          resultMap.put("res", res);
+           response = restTemplate.postForEntity("https://testapi.openbanking.or.kr/v2.0/transfer/withdraw/fin_num", request, String.class);
+           //JSONObject jObject = new JSONObject(response.getBody());
+           
         } catch(Exception e) {
       	  e.printStackTrace();
         }
-        	System.out.println(">> " + resultMap);
+        	System.out.println(">> " + response);
         	
-        return resultMap;
+        return response;
     }
     
     
---
+++ src/main/java/com/board/domain/BankResponseToken.java
@@ -0,0 +1,12 @@
+package com.board.domain;
+import lombok.Data;
+
+@Data
+public class BankReponseToken {
+    private String access_token;
+    private String token_type;
+    private int expires_in;
+    private String refresh_token;
+    private String scope;
+    private String user_seq_no;
+}
